d3b1516cb9e3d45efef6c69620d6feb78286fb01
John C <jcresswell@hotmail.com>
2017-08-07 20:17:36 -0500

    force appveyor

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,7 @@ import {expect} from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
 
+// remove
 describe('Sample test', () => {
     it('should pass', () => {
         expect(true).to.equal(true);
